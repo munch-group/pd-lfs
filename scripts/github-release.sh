f54b8fcb1befc1edf0051604f8d53bfeeccdf492
Kasper Munch <kaspermunch@birc.au.dk>
2026-04-24 11:19:24 +0200

    refactor: clean up github release script by removing commented code

--- scripts/github-release.sh
+++ scripts/github-release.sh
@@ -4,10 +4,6 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-# if [[ `git status --porcelain` ]]; then
-#   echo -e "${RED}Changes must be pushed first.${NC}"
-#   exit
-# else
 v=$(python -c "
 import sys
 try:
@@ -20,7 +16,8 @@ print(data['project']['version'])
 ") || exit
 p=$(python -c "'--prerelease' if 'rc' in \"$v\" else '--latest' ") || exit
 
-# git tag -a "v${v}" -m "${1:-Release}" && git push origin --tags && echo -e "${GREEN}Released version v${v} ${NC}" && exit
-set -v
-gh release create $p "v${v}" --title "v$(python -c "import tomllib; print(tomllib.load(open('pyproject.toml', 'rb'))['project']['version'])")" --notes "" || echo -e "${RED}Failed${NC}" && echo -e "${GREEN}Released version v${v} ${NC}" || echo -e "${RED}Failed${NC}"
-# fi
+repo=$(basename $(git rev-parse --show-toplevel))
+gh repo set-default https://github.com/munch-group/${repo} \
+  && gh release create $p "v${v}" --title "v${v}" --notes "" \
+  && echo -e "${GREEN}Released version v${v} ${NC}" \
+  || echo -e "${RED}Failed${NC}"
